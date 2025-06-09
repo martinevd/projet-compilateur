cf219dd63a15013c70ee272a9f5f8efa5acb171b
martinevd <martin.evrard.private@gmail.com>
2025-06-09 23:56:15 +0200

    Suppression des fichiers inutiles et modification de simple.c

--- simple.c
+++ simple.c
@@ -1,6 +1,21 @@
+void add(int b){
+    int c = b;
+    if (c){
+        add1(b)
+    }
+}
+
+void add1(int b){
+    X = X + 1;
+    add(b - 1)
+}
+
+int X, int Y
+
 void exec(){
-    string c1 = "hello ";
-    string c2 = "world";
-    printf(c1+c2);
-    printf(len(c1))
+    string c1 = "Hello ";
+    string c2 = "World";
+    printf(c1 + c2);
+    add(Y);
+    printf(X)
 }
